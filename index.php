d4265cb74918e6d701dd7dffa16d19e464c94aae
Naitik Parekh <parekhnaitik13@gmail.com>
2017-10-08 21:33:21 +0530

    url

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <html>
 <head>
 
-<meta property="og:title" content="Diwali Greetings" />
-<meta property="og:description" content="Celebrate eco friendly diwali with us." />
-<meta property="og:url" content="http://diwaligreetings.ga" />
-<meta property="og:image" content="http://diwaligreetings.ga/src/image/diwali.jpg" />
 
 <script type="text/javascript">
 function makeShort(longUrl) 
