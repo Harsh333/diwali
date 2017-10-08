119ad7e2e32103cd87a50df3b3107fd2d441c646
hardipraj <rajhardip96@gmail.com>
2017-10-08 18:01:47 +0530

    Shorten URL

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 
 function makeShort(longUrl) 
 {
-
+    console.log(longUrl);
     var request = gapi.client.urlshortener.url.insert({
     'resource': {
       'longUrl': longUrl
@@ -40,9 +40,9 @@ function makeShort(longUrl)
 		
 		if(response.id != null)
 		{
-			str ="<b>Long URL:</b>"+longUrl+"<br>";
-			str +="<b>Short URL:</b> <a href='"+response.id+"'>"+response.id+"</a><br>";
+			str +=response.id;
             document.getElementById("myLink").innerHTML=str;
+            console.log(str);
 			//document.getElementById("output").innerHTML = str;
 		}
 		else
@@ -186,16 +186,16 @@ Diwali Greetings
                 <div style="position: relative; text-align: center; display: none;" id="div2">
                     <form method="get" action="">
                         <input type="text" name="n" style="width: 70%; height:70px; border: 4px solid #2D6525; border-radius: 5px; font-size:25px; font-weight:bold;" placeholder="Enter your name">
-                        <button type="submit" onclick="makeShort(whatsapp://send?text=diwaligreetings.tk/?n=<?php echo $_GET['n']?>)" id="button1" style="height: 70px; background-color: #4CAF50; color: white; border-style: solid; border-width: 5px; border-radius: 10px; border-color: #2D6525; font-size:25px; font-weight:bold;">Create</button>
+                        <button type="submit" onclick="makeShort(http://diwaligreetings.tk/?n=<?php echo $_GET['n']?>)" id="button1" style="height: 70px; background-color: #4CAF50; color: white; border-style: solid; border-width: 5px; border-radius: 10px; border-color: #2D6525; font-size:25px; font-weight:bold;">Create</button>
                     </form>
                 </div>
                 
 				<div style="position: relative; text-align: center; width:100%; margin-top:1%" id="div3">
                     <form method="get" action="" >
                         <button type="submit" style="height: 70px; widht: 100%; background-color: #4CAF50; color: white; border-style: solid; border-width: 5px; border-radius: 10px; border-color: #2D6525; font-size:25px; font-weight:bold;">
-							<a id="mylink" href="diwaligreetings.tk" style="text-decoration: none; color: white;">
+							<a id="mylink" href="whatsapp://send?text=" style="text-decoration: none; color: white;">
 								<div><img src="src\images\whatsapp.png" style="float:left;" height="100%"></div>
-								<div>Share on WhatsApp<div>
+                                <div>Share on WhatsApp</div>
 							</a>
 						</button>
                     </form>
