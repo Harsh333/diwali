d61d287b63e50d735822cf34c890f45cfa7d826a
Naitik Parekh <parekhnaitik13@gmail.com>
2017-10-09 03:21:12 +0530

    Amazon Banner

--- index.php
+++ index.php
@@ -166,6 +166,7 @@ Diwali Greetings
             </td>
         </tr>
 		<tr>
+		<td>
 		<div style="position:relative; widht:inherit;">
 	<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=tf_til&ad_type=product_link&tracking_id=hardipraj0f-21&marketplace=amazon&region=IN&placement=B01MZYMMYI&asins=B01MZYMMYI&linkId=b248b94ae146a9e334952e4e77585664&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066c0&bg_color=ffffff">
     </iframe>
