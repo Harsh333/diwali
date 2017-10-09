c90cecd8e7ac447829d628a50efe230d9e9df7cc
Harsh333 <harshmehta088@gmail.com>
2017-10-10 00:04:00 +0530

    modified cue links

--- index.php
+++ index.php
@@ -126,22 +126,8 @@ body{background:#000;}
 <title>
 Diwali Greetings
 </title>
-    
-    
-<!-- CUELINKS Script -->
-    <script type='text/javascript'>
-    var pubID = '23455CL21105';
-
-    (function(d, t) {
-    var s = document.createElement('script');s.type = 'text/javascript';
-    s.async = true;s.src = (document.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/')  + 'cuelinksv1.js';
-    document.getElementsByTagName('body')[0].appendChild(s);
-    }());
-</script>
-<!-- CUELINKS Script Ends-->    
-
+        
 <body style="overflow: hidden">
-    
     
     <!--Automatic Fireworks --> 
     <canvas id="canvas" style="position: absolute ">Canvas is not supported in your browser.</canvas>
@@ -259,5 +245,19 @@ Diwali Greetings
 -->
 
     <!--Touch fireworks Ends-->
+      
+<!-- CUELINKS Script -->
+    
+    <script type='text/javascript'>
+    var pubID = '23455CL21105';
+
+    (function(d, t) {
+    var s = document.createElement('script');s.type = 'text/javascript';
+    s.async = true;s.src = (document.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/')  + 'cuelinksv1.js';
+    document.getElementsByTagName('body')[0].appendChild(s);
+    }());
+</script>
+
+<!-- CUELINKS Script Ends-->
 </body>
 </html>
