60abaac95896ce77d3edbe99d639668dc6e532d3
hardipraj <rajhardip96@gmail.com>
2017-10-16 14:03:48 +0530

    Lakshmi ads

--- lakshmi.php
+++ lakshmi.php
@@ -185,9 +185,9 @@ Diwali Greetings
 
                                 
 				<div style="position: relative; text-align: center; width:100%; margin-top:1%" id="div3">
-                        <button type="submit" onclick="makeShort('http://diwaligreetings.ga/lakshmi.php')" style="height: 100px; widht: 100%; background-color: #4CAF50; color: white; border-style: solid; border-width: 5px; border-radius: 10px; border-color: #2D6525; font-size:33px; font-weight:bold;">
+                        <button type="submit" style="height: 100px; widht: 100%; background-color: #4CAF50; color: white; border-style: solid; border-width: 5px; border-radius: 10px; border-color: #2D6525; font-size:33px; font-weight:bold;">
 				
-							<a id="mylink2" style="text-decoration: none; color: white;">
+							<a id="mylink2" style="text-decoration: none; color: white;" href="whatsapp://send?text=Is diwali aaie jaane kya hai aapka bhvishya, aap ko kitani lakshmi prapt hogi!\n https://goo.gl/S77mrV">
 								<div><img src="src\images\whatsapp.png" style="float:left;" height="100%"></div>
                                 <div>Share on WhatsApp</div>
 							</a>
